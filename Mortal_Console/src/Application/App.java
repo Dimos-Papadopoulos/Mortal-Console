b06dedcdef6131883c041ae38f1b918be4d1561a
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-03-09 13:43:08 +0200

    More fixes

--- Mortal_Console/src/Application/App.java
+++ Mortal_Console/src/Application/App.java
@@ -5,16 +5,18 @@ import java.util.ArrayList;
 
 public class App {
     public static void entry(){
-        System.out.println("Welcome to Mortal Engines");
+        System.out.println("Welcome to Mortal Console");
         System.out.println("How many players do you wish to have?");
         Scanner scanner = new Scanner(System.in);
         int num = Integer.parseInt(scanner.nextLine());
         getUserNames(num,scanner);
+
+        scanner.close();
     }
     public static ArrayList<String> getUserNames(int num, Scanner scanner){
         ArrayList<String> userNames = new ArrayList<String>();
         for(int i=1;i<=num;i++){
-            System.out.println("Enter Username");
+            System.out.println("Enter username for player " + i);
             userNames.add(scanner.nextLine());
         } 
         return userNames;
