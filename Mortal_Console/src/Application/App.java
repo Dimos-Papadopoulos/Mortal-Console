eb5bf8b4b1dc54f023ec7b41d1bd5973ba2e48b5
Dimos-Papadopoulos <dimos.a.papadopoulos@gmail.com>
2021-03-09 13:36:09 +0200

    Update App.java

--- Mortal_Console/src/Application/App.java
+++ Mortal_Console/src/Application/App.java
@@ -1,8 +1,30 @@
 package Application;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
+    public static void entry(){
+        System.out.println("Welcome to Mortal Engines");
+        System.out.println("How many players do you wish to have?");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        
+        getUserNames(num,scanner);
+    }
+    public static ArrayList<String> getUserNames(int num, Scanner scanner){
+        //Scanner scanner =  new Scanner(System.in);
+        ArrayList<String> userNames = new ArrayList<String>();
+        for(int i=1;i<=num;i++){
+            System.out.println("Enter Username");
+            userNames.add(scanner.nextLine());
+        } 
+        //scanner.close();
+        return userNames;
+    }
+
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        entry();
+        
     }
 }
